2d88096459e858f2d8f65e056f295da8fdce54bf
dpranav301 <31929373+dpranav301@users.noreply.github.com>
2023-09-07 16:40:30 +0000

    Type Conversion

---
+++ Practise/TypeConversion.js
@@ -0,0 +1,17 @@
+let number="10";
+console.log("Type of Original Number=="+typeof number);
+let convertedNumber=Number(number);
+console.log("Type of Conveted Number==",typeof convertedNumber);
+
+console.log("=========================================================");
+
+let bool=true;
+console.log("Type of bool==="+typeof bool);
+let converetdBool=String(bool);
+console.log("Converetd bool=="+converetdBool+"And its value=="+converetdBool);
+
+console.log("=========================================================");
+let x="33abc";
+console.log("Type of x=="+typeof x+"  and its value=="+x);
+let converetdX=Number(x);
+console.log("Type of Converetdx=="+ typeof converetdX+"  and its value=="+converetdX);
