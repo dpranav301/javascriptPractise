3ba772a654ea205e2d71eebdb7fb92ce817b152a
dpranav301 <31929373+dpranav301@users.noreply.github.com>
2023-09-10 16:21:25 +0000

    number library

---
+++ Practise/Strings.js
@@ -0,0 +1,8 @@
+const name="Pranav";
+const job="DGV";
+console.log("My Name is ",name," i am working in ",job);
+console.log(`My name is ${name} and i am working in ${job}`);
+console.log(`My name is ${name.toUpperCase()} and i am working in ${job.toLowerCase()}`);
+const stringObject=new String("Pranav");
+console.log(stringObject);
+console.log("Slice Method=="+stringObject.slice(-4,5));
---
+++ Practise/nums_and_math.js
@@ -0,0 +1,9 @@
+const number=400;
+const num=new Number(number);
+console.log(num);
+console.log(typeof num.toString());
+console.log(num.toFixed(3));
+const otherNumber=121.54;
+console.log(otherNumber.toPrecision(4));
+const otherNumber1=100000;
+console.log(otherNumber1.toLocaleString('en-IN'));
